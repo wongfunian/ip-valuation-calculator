77447af4defc8972838f72a10a5c277edc9e8293
Wong Fu Nian <wongfunian@hotmail.com>
2026-03-12 16:03:00 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
-		port: 3000,
+		port: 3010,
 	},
 	base: '/ip-valuation-calculator/',
 });
